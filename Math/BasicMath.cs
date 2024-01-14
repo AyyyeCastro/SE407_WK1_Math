6118a0b9ec5403782c8540af1d682af86e414d4e
AyyyeJae <97459102+AyyyeJae@users.noreply.github.com>
2024-01-14 17:37:49 -0500

    Page 22/40 of SE407 WK1_Math

---
+++ Math/BasicMath.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Math
+{
+    public class BasicMath
+    {
+
+        public double AddNum(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double SubnNum(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double MultNum(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public double DivNum(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
---
+++ MathTest/BasicMathTest.cs
@@ -0,0 +1,49 @@
+﻿using Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MathTest
+{
+    public class BasicMathTest
+    {
+        [Fact]
+        public void TestAddNum()
+        {
+
+            var math = new BasicMath();
+            var result = math.AddNum(5, 5);
+            Assert.True(result == 10);
+        }
+
+        [Fact]
+        public void TestSubNum()
+        {
+
+            var math = new BasicMath();
+            var result = math.SubnNum(10, 3);
+            Assert.True(result == 7);
+        }
+
+        [Fact]
+        public void TestMultNum()
+        {
+
+            var math = new BasicMath();
+            var result = math.MultNum(5, 2);
+            Assert.True(result == 10);
+        }
+
+        [Fact]
+        public void TestDivNum()
+        {
+
+            var math = new BasicMath();
+            var result = math.DivNum(10, 5);
+            Assert.True(result == 2);
+        }
+    }
+}
